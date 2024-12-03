5a1331cb12abf45185a1f44363b7aa5d72441182
Gregory Noack <gregorynoack@thinknoack.com>
2024-12-03 13:19:46 -0800

    adding baseic styles for now

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import DefaultLayout from "../layouts/DefaultLayout";
-
+import styles from "../styles/Home.module.scss";
 export default function About() {
   return (
     <div className={styles.container}>
